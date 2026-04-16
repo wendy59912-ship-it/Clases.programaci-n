b245f7ede01cc2ae3a0534a47197eac1db9b4918
wendy59912-ship-it <wendy59912@gmail.com>
2026-04-16 09:26:25 -0600

    Add files via upload

---
+++ programa2/src/programa2/Programa2.java
@@ -0,0 +1,13 @@
+
+package programa2;
+public class Programa2 {  
+    public static void main(String[] args) {
+     double altura = 5;
+     double base = 30;
+     double area=base*altura/2;
+     
+     System.out.println("El area del triangulo es"+area);
+     System.out.printf("El area del triangulo es%.3f \n", area);
+    }
+    
+}
